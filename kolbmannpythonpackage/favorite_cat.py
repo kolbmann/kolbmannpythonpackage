62f5f96ea9019fb924c0a2e25ede7dce01d2765d
Christina <christina.kolbmann@selu.edu>
2018-11-15 19:25:27 +0000

    Christina Project 3 with all my scripts

--- kolbmannpythonpackage/favorite_cat.py
+++ kolbmannpythonpackage/favorite_cat.py
@@ -1,6 +1,18 @@
+"""
+Paramaters 
+-------------------------------------------------------
+No input is needed. Leave arguments blank.
+
+Returns
+------------------------------------------------------------------
+Prints a random cat name from list of wildcats with message
+"""
+
+
 import random 
 wildcats = ['A Bobcat', 'A Canada Lynx', 'A Eurasian Lynx', 'A Siberian Lynx', 'An Ocelot', 'A Margay', 'A Jaguarundi', 'A Geoffreys', 'A Serval', 'A Black Footed Cat', 'A Fishing Cat', 'A Caracal', 'A Kodkod', 'A Marbled Cat', 'A Sandcat']
 
 def favorite_cat():
     print(random.choice(wildcats) + " is my favorite cat today!")
     print("**Just kidding they're all my favorite**")
+    assert (len(wildcats) >= 0)
